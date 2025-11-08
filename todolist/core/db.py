a023c9adf5de8bcae3bfcde4fc9483c7aa2b6088
NarjesAghahasani <narjesaghahasani@gmail.com>
2025-11-08 16:06:58 +0330

    chore: scaffold app package structure for phase 2

--- todolist/core/db.py
+++ todolist/core/db.py
@@ -10,7 +10,7 @@ load_dotenv()
 DB_USER = os.getenv("DB_USER")
 DB_PASSWORD = os.getenv("DB_PASSWORD")
 DB_HOST = os.getenv("DB_HOST")
-DB_PORT =سس os.getenv("DB_PORT")
+DB_PORT = os.getenv("DB_PORT")
 DB_NAME = os.getenv("DB_NAME")
 
 # Build the database URL
